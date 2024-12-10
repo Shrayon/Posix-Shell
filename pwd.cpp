f622c8156e2f45fd7451bdcbbaabbc3d89ddebe1
Shrayon <141469890+Shrayon@users.noreply.github.com>
2024-12-10 23:47:31 +0530

    Add files via upload

---
+++ pwd.cpp
@@ -0,0 +1,20 @@
+#include <ostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+using namespace std;
+
+void dopwdstuff(ostream &output_stream) {
+  char chunk[8192];
+  getcwd(chunk, sizeof(chunk));
+  output_stream << chunk << endl;
+  
+}
+string quickpwd() {
+  char chunk[8192];
+  getcwd(chunk, sizeof(chunk));
+  string ret(chunk);
+  return ret;
+  
+}
